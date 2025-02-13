a6efeb7ac01733437949cc551de22b4fc773ed65
HarrisonHakanen <github@positioninvest.com>
2025-02-13 12:00:20 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -13,9 +13,19 @@ setup(
                         + 'Simple and intuitive, Routine allows anyone to schedule tasks quickly and '
                         + 'efficiently—without any hassle. '
                         
-    url = 'https://github.com/yanorestes/aluratemp',
+    url = 'https://github.com/HarrisonHakanen/routine',
     project_urls = {
-        'Código fonte': 'https://github.com/yanorestes/aluratemp',
-        'Download': 'https://github.com/yanorestes/aluratemp/archive/1.0.0.zip'
-    }
+        'Código fonte': 'https://github.com/HarrisonHakanen/routine',
+        'Download': 'https://github.com/HarrisonHakanen/routine/archive/refs/heads/main.zip'
+    },
+    license = 'MIT',
+    keywords = 'routine schedule job jobs cron',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization'
+    ]
 )
